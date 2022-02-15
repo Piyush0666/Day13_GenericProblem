ed4dd4adb86d5340bb7fa6d71d59b932474cedb4
your_github_username <your_github_email>
2022-02-15 08:17:34 +0530

    Generics to find max values

---
+++ src/test/java/com/bridgelabz/Generic/day13/maximumTest.java
@@ -0,0 +1,25 @@
+package com.bridgelabz.Generic.day13;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class maximumTest {
+    @Test
+    public void findMaxFromThreeIntegersValues() {
+        Integer Int1 = 10, Int2 = 12, Int3 = 7;
+        Integer intMax = Operation.testMaximum(Int1, Int2, Int3);
+        Assertions.assertEquals(12, intMax);
+    }
+    @Test
+    public void findMaxFromThreeFloatValues() {
+        Float floatValue1 = 2.7f, floatValue2 = 19.3f, floatValue3 = 7.5f;
+        Float floatMax = Operation.testMaximum(floatValue1, floatValue2, floatValue3);
+        Assertions.assertEquals(19.3f, floatMax);
+    }
+    @Test
+    public void findMaxFromThreeStringValues() {
+        String xString = "Cherry", yString = "Banana", zString = "Apple";
+        String stringMax = Operation.testMaximum(xString, yString, zString);
+        Assertions.assertEquals("Cherry", xString);
+    }
+}
